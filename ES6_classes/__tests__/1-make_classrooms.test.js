3943bdc38be6e4d78c83a6a3aa8e46ffa970ef35
Salomée Perton <pertonsalomee@gmail.com>
2025-03-26 09:28:59 +0100

    1

---
+++ ES6_classes/__tests__/1-make_classrooms.test.js
@@ -0,0 +1,17 @@
+import initializeRooms from '../1-make_classrooms.js';
+import ClassRoom from '../0-classroom.js';
+
+describe('Task 1 - initializeRooms', () => {
+	test('should return an array of 3 ClassRoom instances', () => {
+		const rooms = initializeRooms();
+
+		expect(rooms).toHaveLength(3);
+		expect(rooms[0]).toBeInstanceOf(ClassRoom);
+		expect(rooms[1]).toBeInstanceOf(ClassRoom);
+		expect(rooms[2]).toBeInstanceOf(ClassRoom);
+
+		expect(rooms[0]._maxStudentsSize).toBe(19);
+		expect(rooms[1]._maxStudentsSize).toBe(20);
+		expect(rooms[2]._maxStudentsSize).toBe(34);
+	});
+});
