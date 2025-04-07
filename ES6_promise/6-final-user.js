698aa80cc469cc7b72f855032aa538ad529f93f0
Salomée Perton <pertonsalomee@gmail.com>
2025-04-07 11:51:44 +0200

    fixing 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,22 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    uploadPhoto(fileName),
-    signUpUser(firstName, lastName),
-  ]);
+	return Promise.allSettled([
+		uploadPhoto(fileName),
+		signUpUser(firstName, lastName),
+	]).then((results) =>
+		results.map((result) => {
+			if (result.status === 'fulfilled') {
+				return {
+					status: result.status,
+					value: result.value,
+				};
+			}
+			return {
+				status: result.status,
+				value: result.reason.toString(),
+			};
+		})
+	);
 }
