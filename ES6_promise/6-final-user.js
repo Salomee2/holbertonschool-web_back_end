0b3470d80ea80409ed6a185f8270b7504bb377c3
Salomée Perton <pertonsalomee@gmail.com>
2025-04-07 11:59:15 +0200

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,21 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	return Promise.allSettled([
-		uploadPhoto(fileName),
-		signUpUser(firstName, lastName),
-	]).then((results) =>
-		results.map((result) => {
-			if (result.status === 'fulfilled') {
-				return {
-					status: result.status,
-					value: result.value,
-				};
-			}
-			return {
-				status: result.status,
-				value: result.reason.toString(),
-			};
-		})
-	);
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+    return {
+      status: result.status,
+      value: result.reason.toString(),
+    };
+  }));
 }
--- ES6_promise/__tests__/6-final-user.test.js
+++ ES6_promise/__tests__/6-final-user.test.js
@@ -1,14 +1,21 @@
 import handleProfileSignup from '../6-final-user.js';
 
 describe('handleProfileSignup', () => {
-	test('should return results of all promises regardless of outcome', async () => {
-		const results = await handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg');
+	test('returns correct structure with rejected and fulfilled results', async () => {
+		const result = await handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg');
 
-		expect(results).toHaveLength(2);
-		expect(results[0].status).toBe('rejected');
-		expect(results[0].reason).toEqual(new Error('bob_dylan.jpg cannot be processed'));
-
-		expect(results[1].status).toBe('fulfilled');
-		expect(results[1].value).toEqual({ firstName: 'Bob', lastName: 'Dylan' });
+		expect(result).toEqual([
+			{
+				status: 'rejected',
+				value: 'Error: bob_dylan.jpg cannot be processed',
+			},
+			{
+				status: 'fulfilled',
+				value: {
+					firstName: 'Bob',
+					lastName: 'Dylan',
+				},
+			},
+		]);
 	});
 });
