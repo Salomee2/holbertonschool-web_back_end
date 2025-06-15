4d6fe326a8a4e407621433e47f693536d07623c4
Salomée Perton <pertonsalomee@gmail.com>
2025-06-15 21:57:06 +0200

    t4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,9 @@ list of delays sorted in ascending order without using sort().
 
 import asyncio
 from typing import List
-from 3-tasks import task_wait_random
+
+# Import task_wait_random from the module '3-tasks'
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
